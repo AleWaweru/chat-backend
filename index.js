1b934fa6a2bdf86c67050c0ad54c08531b627f5e
AleWaweru <ngashalex8@gmail.com>
2024-03-26 10:54:07 +0300

    database

--- index.js
+++ index.js
@@ -1,29 +1,25 @@
-const express = require('express');
-const cors = require("cors");
+const express = require("express");
 const app = express();
+const cors = require("cors");
 require("dotenv").config();
 
 app.use(express.json());
 app.use(cors());
+
 const db = require("./models");
 
-//routers
-const postRouter = require('./routes/Posts')
+// Routers
+const postRouter = require("./routes/Posts");
 app.use("/posts", postRouter);
-
-const commentRouter = require('./routes/Comments')
-app.use("/comments", commentRouter);
-
-const usersRouter = require('./routes/Users')
+const commentsRouter = require("./routes/Comments");
+app.use("/comments", commentsRouter);
+const usersRouter = require("./routes/Users");
 app.use("/auth", usersRouter);
-
-const likesRouter = require('./routes/Likes')
+const likesRouter = require("./routes/Likes");
 app.use("/likes", likesRouter);
 
 db.sequelize.sync().then(() => {
-    app.listen(process.env.PORT || 3001, () => {
-        console.log("Server running on port 3001");
-    });
-}).catch((err) =>{
- console.log(err);
+  app.listen(process.env.PORT || 3001, () => {
+    console.log("Server running on port 3001");
+  });
 });
